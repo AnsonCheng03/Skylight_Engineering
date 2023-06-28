f769f1cd260084f1e2e66ab378af415ce712d01f
Anson Cheng <Anson.12666@icloud.com>
2023-06-29 04:17:04 +0800

    chore: edit header link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -14,17 +14,38 @@ export default component$(() => {
       <div class={styles.blur}></div>
       <ul class={styles.menu}>
         <li class={styles.menuItem}>
-          <a href="/" class={styles.link}>
+          <a
+            href={
+              process.env.NODE_ENV !== "development"
+                ? "/"
+                : "/Skylight_Engineering"
+            }
+            class={styles.link}
+          >
             首頁
           </a>
         </li>
         <li class={styles.menuItem}>
-          <a href="/example" class={styles.link}>
+          <a
+            href={
+              process.env.NODE_ENV !== "development"
+                ? "/about-us"
+                : "/Skylight_Engineering/about-us"
+            }
+            class={styles.link}
+          >
             裝修案例
           </a>
         </li>
         <li class={styles.menuItem}>
-          <a href="/contact" class={styles.link}>
+          <a
+            href={
+              process.env.NODE_ENV !== "development"
+                ? "/contact"
+                : "/Skylight_Engineering/contact"
+            }
+            class={styles.link}
+          >
             聯絡我們
           </a>
         </li>
