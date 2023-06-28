2c92b87d2f60d39505100df5ba33fda127d4959a
Anson Cheng <Anson.12666@icloud.com>
2023-06-29 04:49:04 +0800

    chore: static adapter origin

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: 'https://skylightengineering.com.hk/',
+        origin: 'https://ansoncheng03.github.io/Skylight_Engineering/',
       }),
     ],
   };
