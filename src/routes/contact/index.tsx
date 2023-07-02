629bd59a352413e48fa9032afff7978171e2f89e
Anson Cheng <Anson.12666@icloud.com>
2023-07-03 00:48:59 +0800

    fix: remove unused dep and css

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 import coverImage from "./images/cover.jpg";
 import styles from "./contact.module.css";
 
