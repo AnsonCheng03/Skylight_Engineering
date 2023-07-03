8e4a059ee85f1063d6a0d1cc02bb56d1c28be7cf
Anson Cheng <Anson.12666@icloud.com>
2023-07-03 21:12:53 +0800

    feat: full screen example slideshow with image UI

--- src/components/slideShow/slideShow.tsx
+++ src/components/slideShow/slideShow.tsx
@@ -14,35 +14,31 @@ export default component$(({ photos, fullScreenSlideshow }: any) => {
         </div>
         <div class={styles.slideshowContainer}>
           <div class={styles.slideshow}>
-            {/* 
-            photo.Images.forEach((image) => {
-              const img = document.createElement("img");
-              img.src = photo.path + "/" + image;
-              img.addEventListener("click", () => {
-                if (img.classList.contains("active")) {
-                  //remove active class
-                  img.classList.remove("active");
-                  img.parentElement.classList.remove("active");
-                  //scroll to the photo
-                  // img.parentElement.scrollTo({
-                  //   top: img.offsetTop,
-                  //   left: 0,
-                  //   behavior: "smooth",
-                  // });
-                } else {
-                  img.classList.add("active");
-                  img.parentElement.classList.add("active");
-                }
-              });
-              slideshow.appendChild(img);
-            }); 
-    */}
-            {photos.forEach((photo: any) => {
-              return (
-                <div class={styles.slide}>
-                  <img src={photo.path + "/" + photo.Images[0]} />
-                </div>
-              );
+            {photos.map((photo: any) => {
+              if (photo.name === fullScreenSlideshow.value)
+                return photo.Images.map((image: any) => {
+                  return (
+                    <img
+                      src={photo.path + "/" + image}
+                      onClick$={() => {
+                        //                       if (img.classList.contains("active")) {
+                        //   //remove active class
+                        //   img.classList.remove("active");
+                        //   img.parentElement.classList.remove("active");
+                        //   //scroll to the photo
+                        //   // img.parentElement.scrollTo({
+                        //   //   top: img.offsetTop,
+                        //   //   left: 0,
+                        //   //   behavior: "smooth",
+                        //   // });
+                        // } else {
+                        //   img.classList.add("active");
+                        //   img.parentElement.classList.add("active");
+                        //                       }
+                      }}
+                    />
+                  );
+                });
             })}
           </div>
         </div>
