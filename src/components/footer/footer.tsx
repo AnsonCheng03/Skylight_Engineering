d269f4505e9330a3e573361d3483e7296994211b
Anson Cheng <Anson.12666@icloud.com>
2023-07-04 04:29:46 +0800

    feat: add icons on footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,48 @@
 import { component$ } from "@builder.io/qwik";
+import { BsTelephone, BsWhatsapp, BsInstagram } from "@qwikest/icons/bootstrap";
+import { GoMail24 } from "@qwikest/icons/octicons";
 import styles from "./footer.module.css";
 
 export default component$(() => {
   return (
     <footer class={styles.footer}>
-      <h3 class={styles.title}>天晴工程有限公司</h3>
+      <div class={styles.container}>
+        <h3 class={styles.title}>天晴工程有限公司</h3>
+        <div class={styles.icons}>
+          <BsTelephone
+            class={styles.icon}
+            onClick$={() => {
+              window.open("tel:+85290732506", "_blank");
+            }}
+          />
+          <BsWhatsapp
+            class={styles.icon}
+            onClick$={() => {
+              window.open("https://wa.me/85290732506", "_blank");
+            }}
+          />
+          <GoMail24
+            class={styles.icon}
+            onClick$={() => {
+              window.open(
+                "mailto:contact@skylightengineering.com.hk",
+                "_blank"
+              );
+            }}
+          />
+          <BsInstagram
+            class={styles.icon}
+            onClick$={() => {
+              window.open(
+                "https://www.instagram.com/skylight_engg_901/",
+                "_blank"
+              );
+            }}
+          />
+        </div>
+      </div>
       <hr class={styles.line} />
-      <div class={styles.extrainfo}>
+      <div class={styles.extraInfo}>
         <div>
           <p class={styles.subtitle}>九龍官塘觀塘鴻圖道42號華寶中心9樓901室</p>
         </div>
