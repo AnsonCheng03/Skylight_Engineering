6f88752b6cc79b61055e36c77fee61698e182fd2
Anson Cheng <Anson.12666@icloud.com>
2023-07-04 05:19:00 +0800

    chore: update whatsapp icon

--- src/components/floatingContact/floatingContact.tsx
+++ src/components/floatingContact/floatingContact.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { BsWhatsapp } from "@qwikest/icons/bootstrap";
+import { SiWhatsapp } from "@qwikest/icons/simpleicons";
 import styles from "./floatingContact.module.css";
 import { Link } from "@builder.io/qwik-city";
 
@@ -8,8 +8,7 @@ export default component$(() => {
     <div class={styles.contactFloat}>
       <div class={styles.whatsapp}>
         <Link href="https://wa.me/85290732506" target="_blank">
-          {/* <img src={icon} alt="whatsapp" width={35} height={35} /> */}
-          <BsWhatsapp class={styles.icon} />
+          <SiWhatsapp class={styles.icon} />
         </Link>
       </div>
     </div>
