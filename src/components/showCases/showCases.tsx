df687ca07247a5973e3042eb78987234e2ea4d43
Anson Cheng <Anson.12666@icloud.com>
2023-07-03 20:31:30 +0800

    feat: example cases UI

---
+++ src/components/showCases/showCases.tsx
@@ -0,0 +1,171 @@
+import { component$, $ } from "@builder.io/qwik";
+import styles from "./showCases.module.css";
+import cameraIcon from "./assets/camera.png";
+import copyLinkIcon from "./assets/copy-link.png";
+import shareIcon from "./assets/share.png";
+
+export default component$(({ photos, type, name }: any) => {
+  const activeType = type || photos[0].type;
+  const caseTypes = photos.map((photo: any) => photo.type);
+  const distinctCaseTypes = [...new Set(caseTypes)];
+
+  const copyLink = $((type: any, name: any) => {
+    //Create a temporary input element
+    const tempInput = document.createElement("input");
+    const url = window.location.href.split("?")[0];
+    tempInput.value = url + "?type=" + type + "&name=" + name;
+    document.body.appendChild(tempInput);
+
+    //Select the input element
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999);
+
+    //Copy the input element
+    document.execCommand("copy");
+
+    //Remove the input element
+    document.body.removeChild(tempInput);
+
+    //Show the copy-link button
+    sharingButton.classList.add("active");
+    setTimeout(() => {
+      sharingButton.classList.remove("active");
+    }, 2000);
+  });
+
+  const shareLink = $((photo: any) => {
+    const url = window.location.href.split("?")[0];
+    const shareData = {
+      title: photo.name,
+      text: photo.name + "\n\n" + photo.Text + "\n\n" + "按此查看更多：",
+      url: url + "?type=" + photo.type + "&name=" + photo.name,
+    };
+    navigator.share(shareData);
+  });
+
+  const jumpToCaseByQuery = $((name: any) => {
+    if (name) {
+      const cases = document.querySelectorAll(".case");
+      cases.forEach((caseItem) => {
+        if (caseItem.querySelector("h3")?.textContent == name) {
+          //scroll to case below navigation bar
+          window.scrollTo({
+            top: caseItem.offsetTop - 125,
+            behavior: "smooth",
+          });
+          return;
+        }
+      });
+    }
+  });
+
+  return (
+    <section class={styles.cases}>
+      <section class={styles.selectCase}>
+        <div class={styles.container}>
+          {distinctCaseTypes.map((type: any) => {
+            return (
+              <div
+                class={
+                  type == activeType
+                    ? [styles.caseType, styles.active]
+                    : styles.caseType
+                }
+                key={type}
+                onClick$={() => {
+                  // //Remove active class
+                  // const activeCaseType =
+                  //   document.querySelector(".case-type.active");
+                  // activeCaseType.classList.remove("active");
+                  // //Add active class
+                  // caseType.classList.add("active");
+                  // //Remove all cases
+                  // const displayCase = document.querySelector(".display-case");
+                  // displayCase.innerHTML = "";
+                }}
+              >
+                <h3>{type}</h3>
+                <div class={styles.caseTypeNavigation}>
+                  {photos.map((photo: any) => {
+                    return (
+                      photo.type == type && (
+                        <h3
+                          onClick$={() => {
+                            //Jump to the case
+                            jumpToCaseByQuery(photo.name);
+                          }}
+                        >
+                          {photo.name}
+                        </h3>
+                      )
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section class={styles.displayCase}>
+        {photos.map((photo: any) => {
+          return (
+            photo.type == activeType && (
+              <div class={styles.case}>
+                <h3>{photo.name}</h3>
+                <div class={styles.caseDetails}>
+                  <div
+                    class={styles.photoSelector}
+                    onClick$={() => {
+                      displaySlideshow(photo);
+                    }}
+                  >
+                    <img class={styles.cover} src={cameraIcon} />
+                    <img
+                      class={styles.background}
+                      src={photo.path + "/" + photo.Cover}
+                    />
+                  </div>
+                  <div class={styles.caseDescription}>
+                    {photo.Text.split("\n").map((text: any) => {
+                      return <p>{text}</p>;
+                    })}
+                  </div>
+                  <div class={styles.buttonContainer}>
+                    <button
+                      class={styles.quotationButton}
+                      onClick$={() => {
+                        // send the quotation request via whatsapp
+                        const message = `你好，我想查詢${photo.name}的報價。`;
+                        const whatsAppNumber = "85290732506";
+                        const url = `https://api.whatsapp.com/send?phone=${whatsAppNumber}&text=${message}`;
+                        window.open(url, "_blank");
+                      }}
+                    >
+                      報價
+                    </button>
+                    <button
+                      class={styles.sharingButton}
+                      onClick$={() => {
+                        copyLink(photo.type, photo.name);
+                      }}
+                    >
+                      <img src={copyLinkIcon} />
+                    </button>
+                    <button
+                      class={styles.sharingButton}
+                      onClick$={() => {
+                        shareLink(photo);
+                      }}
+                    >
+                      <img src={shareIcon} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
+          );
+        })}
+      </section>
+    </section>
+  );
+});
--- src/routes/example/index.tsx
+++ src/routes/example/index.tsx
@@ -1,24 +1,42 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal } from "@builder.io/qwik";
+import { routeLoader$, useLocation } from "@builder.io/qwik-city";
+import ShowCases from "~/components/showCases/showCases";
+import SlideShow from "~/components/slideShow/slideShow";
+import styles from "./example.module.css";
+
+export const useImageSource = routeLoader$(async (requestEvent) => {
+  const res = await fetch(
+    requestEvent.url.origin + "/Skylight_Engineering/Images.json",
+    { cache: "no-store" }
+  );
+  try {
+    const data = await res.json();
+    const modifiedData = data.map((item: any) => ({
+      ...item,
+      path: requestEvent.url.origin + "/Skylight_Engineering" + item.path,
+    }));
+    return modifiedData;
+  } catch (error) {
+    return [[]];
+  }
+});
 
 export default component$(() => {
+  const location = useLocation();
+  const imageSource = useImageSource().value;
+  const type = location.url.searchParams.get("type");
+  const name = location.url.searchParams.get("name");
+
   return (
     <>
-      <section class="banner">
-        <h2>裝修案例</h2>
-        <p class="tag"></p>
+      <section class={styles.banner}>
+        <h2 class={styles.h2}>裝修案例</h2>
+        <p class={styles.tag}>{imageSource.length}</p>
       </section>
 
-      <section class="cases">
-        <section class="select-case"></section>
-        <section class="display-case"></section>
-      </section>
+      <ShowCases photos={imageSource} type={type} name={name} />
 
-      <div class="full-screen-slideshow">
-        <div class="close-button">×</div>
-        <div class="slideshow-container">
-          <div class="slideshow"></div>
-        </div>
-      </div>
+      <SlideShow photos={imageSource} />
     </>
   );
 });
